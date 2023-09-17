8fd822bb241688c13f750370355603f39c70ee9f
한상훈 <hzyan2@gmail.com>
2023-09-17 22:22:09 +0900

    todoList V1 start

---
+++ src/main/java/com/todo/todolist/domain/controller/TodoV1Controller.java
@@ -0,0 +1,26 @@
+package com.todo.todolist.domain.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class TodoV1Controller {
+
+
+    // select
+
+
+    // insert
+
+
+    // update
+
+
+    // delete
+
+
+
+}
