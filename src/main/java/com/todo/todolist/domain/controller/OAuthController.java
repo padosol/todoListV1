640b3353cebfeb50763262789ac00eab674d5609
한상훈 <hzyan2@gmail.com>
2023-11-11 18:44:24 +0900

    oauath 구현중

--- src/main/java/com/todo/todolist/domain/controller/OAuthController.java
+++ src/main/java/com/todo/todolist/domain/controller/OAuthController.java
@@ -3,9 +3,10 @@ package com.todo.todolist.domain.controller;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.todo.todolist.domain.service.OAuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping("/login/oauth2")
 @RequiredArgsConstructor
 public class OAuthController {
@@ -13,14 +14,14 @@ public class OAuthController {
     private final OAuthService oAuthService;
 
     @GetMapping("/code/{registrationId}")
-    public String googleLogin(@RequestParam String code, @PathVariable String registrationId) {
+    public JsonNode googleLogin(@RequestParam String code, @PathVariable String registrationId) {
         System.out.println(code);
         System.out.println(registrationId);
 
         String accessToken = oAuthService.getAccessToken(code, registrationId);
         JsonNode userResource = oAuthService.getUserResource(accessToken);
 
-        return accessToken;
+        return userResource;
     }
 
 
