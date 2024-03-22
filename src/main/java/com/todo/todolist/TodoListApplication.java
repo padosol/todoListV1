5522177e334153fa670d468bec41c4c4b6d94d40
한상훈 <hsh@argonet.co.kr>
2024-03-22 12:27:31 +0900

    테스트

--- src/main/java/com/todo/todolist/TodoListApplication.java
+++ src/main/java/com/todo/todolist/TodoListApplication.java
@@ -8,7 +8,7 @@ public class TodoListApplication {
 
     public static void main(String[] args) {
 
-        System.out.println("CI / CD 5");
+        System.out.println("CI / CD");
 
         SpringApplication.run(TodoListApplication.class, args);
     }
