a87a33ee7f23c5c46f81082d9c1d1686f633d395
한상훈 <hzyan2@gmail.com>
2024-03-22 09:54:56 +0900

    3

--- src/main/java/com/todo/todolist/TodoListApplication.java
+++ src/main/java/com/todo/todolist/TodoListApplication.java
@@ -8,7 +8,7 @@ public class TodoListApplication {
 
     public static void main(String[] args) {
 
-        System.out.println("CI / CD 2");
+        System.out.println("CI / CD 3");
 
         SpringApplication.run(TodoListApplication.class, args);
     }
